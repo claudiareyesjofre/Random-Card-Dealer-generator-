985f4ce25948dd22ced79b3c6b296e5358cc8d3f
claudiareyesjofre <claudia.reyes.joffre@gmail.com>
2022-10-29 17:07:28 +0000

    se genera fondo y iconos aleatorio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#card").innerHTML = generateRandomNumber();
+  });
+  console.log();
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let suit = ["♦", "♥", "♠", "♣"];
+
+  let indexnumbers = Math.floor(Math.random() * numbers.length);
+  let indexsuit = Math.floor(Math.random() * suit.length);
+  return suit[indexsuit] + numbers[indexnumbers] + suit[indexsuit];
 };
